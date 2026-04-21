3fb50ea2f67180e0cf4897d9cff09a656d86e188
YashPatel4268 <142138437+YashPatel4268@users.noreply.github.com>
2026-04-21 18:00:14 +0530

    Added Users Export to Excel with AdminLTE integration and role formatting

--- resources/views/vendor/boilerplate/users/list.blade.php
+++ resources/views/vendor/boilerplate/users/list.blade.php
@@ -7,20 +7,36 @@
 ])
 
 @section('content')
-    <div class="row">
-        <div class="col-12 mbl">
-            <span class="float-right pb-3">
-                <a href="{{ route("boilerplate.users.create") }}" class="btn btn-primary">
-                    @lang('boilerplate::users.create.title')
-                </a>
-            </span>
-        </div>
+
+<div class="row">
+    <div class="col-12 mb-3 d-flex justify-content-between">
+
+        <!-- Create User Button -->
+        <a href="{{ route('boilerplate.users.create') }}" class="btn btn-primary">
+            @lang('boilerplate::users.create.title')
+        </a>
+
+        <!-- Export Users Button -->
+        <a href="{{ route('boilerplate.users.export') }}" class="btn btn-success">
+            <i class="fas fa-file-excel"></i> Export Users
+        </a>
+
     </div>
-    @component('boilerplate::card')
-        @component('boilerplate::datatable', ['name' => 'users']) @endcomponent
+</div>
+
+<!-- Users Table -->
+@component('boilerplate::card')
+    @component('boilerplate::datatable', ['name' => 'users'])
     @endcomponent
+@endcomponent
+
 @endsection
 
+{{-- Optional CSS (SAFE) --}}
 @push('css')
-    <style>.img-circle { border:1px solid #CCC }</style>
+<style>
+    .img-circle {
+        border: 1px solid #ccc;
+    }
+</style>
 @endpush
